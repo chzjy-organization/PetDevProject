26b7a2f787e79c3dd05e5b12a19679a7588ecce7
han.chen <han.chen@husor.com>
2019-08-17 10:01:19 +0800

    代码提交

---
+++ app/src/main/java/com/punuo/sys/app/MainActivity.java
@@ -0,0 +1,18 @@
+package com.punuo.sys.app;
+
+import android.os.Bundle;
+
+import com.punuo.sys.sdk.activity.BaseActivity;
+
+/**
+ * Created by han.chen.
+ * Date on 2019-08-17.
+ **/
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
