8a3ffe18a98aedc0bb296ee9f7964a5571043dbc
Kuiya <1144573342@qq.com>
2019-10-16 10:53:34 +0800

    云台测试

---
+++ app/src/main/java/com/punuo/sys/app/Turn.java
@@ -0,0 +1,47 @@
+package com.punuo.sys.app;
+
+import android.util.Log;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Turn {
+
+    public Boolean turnRight(){
+        String filepath = "/dev/petfoodweighter";
+        String cmd = "1";
+
+        try{
+            FileOutputStream fos = new FileOutputStream(filepath);
+            fos.write(cmd.getBytes());
+            fos.flush();
+            fos.close();
+        }catch(IOException e){
+            Log.e("SerialService", "++liujihui++, IOException: "+e);
+            return false;
+        }
+
+//        try {
+//            Process process = Runtime.getRuntime().exec("pet_right");
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+        return true;
+    }
+
+    public Boolean turnStop(){
+        String filepath = "/dev/petfoodweighter";
+        String cmd = "0";
+
+        try{
+            FileOutputStream fos = new FileOutputStream(filepath);
+            fos.write(cmd.getBytes());
+            fos.flush();
+            fos.close();
+        }catch(IOException e){
+            Log.e("SerialService", "++liujihui++, IOException: "+e);
+            return false;
+        }
+        return true;
+    }
+}
