da6d606698f72a21fdaef5040ac6b3968627b331
wu <757169897@qq.com>
2019-10-25 15:01:37 +0800

    开机自启。WiFi键控制蓝牙

--- app/src/main/java/com/punuo/sys/app/led/ButtonBroadcast.java
+++ app/src/main/java/com/punuo/sys/app/led/ButtonBroadcast.java
@@ -1,18 +1,28 @@
 package com.punuo.sys.app.led;
 
+import android.bluetooth.BluetoothAdapter;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.widget.Toast;
-
 import org.greenrobot.eventbus.EventBus;
 
 
 public class ButtonBroadcast extends BroadcastReceiver {
 LedData ledData=new LedData();
+private BluetoothAdapter mBluetooth;
+
     @Override
     public void onReceive(Context context, Intent intent) {
-        Toast.makeText(context,"you click button",Toast.LENGTH_SHORT).show();
+        mBluetooth=BluetoothAdapter.getDefaultAdapter();
         EventBus.getDefault().post(ledData);
+        if(!mBluetooth.isEnabled()){
+            mBluetooth.enable();
+            Toast.makeText(context,"蓝牙打开",Toast.LENGTH_SHORT).show();
+        }
+        if(mBluetooth.isEnabled()){
+            mBluetooth.disable();
+            Toast.makeText(context,"蓝牙关闭",Toast.LENGTH_SHORT).show();
+        }
     }
 }
